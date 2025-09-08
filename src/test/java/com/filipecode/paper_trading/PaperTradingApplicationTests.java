5605a344181ab39a08903377dc7955eb25b3950f
Filipe Mota <filipera3@gmail.com>
2025-09-08 20:09:26 -0300

    first commit

---
+++ src/test/java/com/filipecode/paper_trading/PaperTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipecode.paper_trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
