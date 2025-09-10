253aeca3d6e7b119ff3a903224815cbe9eebbf07
Filipe Mota <filipera3@gmail.com>
2025-09-10 13:28:53 -0300

    setting security config for h2 database

--- src/main/java/com/filipecode/papertrading/infrastructure/config/security/DevSecurityConfig.java
+++ src/main/java/com/filipecode/papertrading/infrastructure/config/security/DevSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,7 +16,9 @@ public class DevSecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll()
                 )
-        .csrf(csrf -> csrf.disable());
+        .csrf(csrf -> csrf.disable())
+        .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
         return http.build();
     }
 }
