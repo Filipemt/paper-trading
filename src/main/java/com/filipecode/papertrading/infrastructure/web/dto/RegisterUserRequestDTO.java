22a2c8116e05a0788c98f978823dae515cd34968
Filipe Mota <filipera3@gmail.com>
2025-09-10 13:28:37 -0300

    add message in validations

--- src/main/java/com/filipecode/papertrading/infrastructure/web/dto/RegisterUserRequestDTO.java
+++ src/main/java/com/filipecode/papertrading/infrastructure/web/dto/RegisterUserRequestDTO.java
@@ -4,8 +4,18 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record RegisterUserRequestDTO(@NotBlank String name,
-                                     @NotBlank @Email String email,
-                                     @NotBlank @Size(min = 8) String password,
-                                     @NotBlank String cpf) {
+public record RegisterUserRequestDTO(
+        @NotBlank(message = "O nome não pode estar em branco.")
+        String name,
+
+        @NotBlank(message = "O E-mail não pode estar em branco.")
+        @Email(message = "E-mail fornecido não é válido.")
+        String email,
+
+        @NotBlank(message = "A senha não pode estar em branco.")
+        @Size(min = 8, message = "Senha deve possuir no mínimo 8 caracteres.")
+        String password,
+
+        @NotBlank(message = "CPF não pode estar em branco.") String cpf
+) {
 }
