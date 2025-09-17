fc9fe75bddb0fa8ade83f45646e859a994997901
Filipe Mota <filipera3@gmail.com>
2025-09-17 08:42:12 -0300

    add migration initial assets with STOCKs and FIIs

---
+++ src/main/resources/db/migration/V2__insert_initial_assets.sql
@@ -0,0 +1,22 @@
+-- Inserindo 10 Ações Brasileiras
+INSERT INTO assets (ticker, company_name, type) VALUES
+                                                    ('PETR4', 'Petróleo Brasileiro S.A. - Petrobras', 'STOCK'),
+                                                    ('VALE3', 'Vale S.A.', 'STOCK'),
+                                                    ('ITUB4', 'Itaú Unibanco Holding S.A.', 'STOCK'),
+                                                    ('BBDC4', 'Banco Bradesco S.A.', 'STOCK'),
+                                                    ('ABEV3', 'Ambev S.A.', 'STOCK'),
+                                                    ('MGLU3', 'Magazine Luiza S.A.', 'STOCK'),
+                                                    ('WEGE3', 'WEG S.A.', 'STOCK'),
+                                                    ('B3SA3', 'B3 S.A. - Brasil, Bolsa, Balcão', 'STOCK'),
+                                                    ('SUZB3', 'Suzano S.A.', 'STOCK'),
+                                                    ('GGBR4', 'Gerdau S.A.', 'STOCK'),
+                                                    ('MXRF11', 'Maxi Renda Fundo de Investimento Imobiliário', 'FII'),
+                                                    ('KNRI11', 'Kinea Renda Imobiliária Fundo de Investimento Imobiliário', 'FII'),
+                                                    ('BCFF11', 'BTG Pactual Fundo de Fundos FII', 'FII'),
+                                                    ('XPLG11', 'XP Log Fundo de Investimento Imobiliário', 'FII'),
+                                                    ('VISC11', 'Vinci Shopping Centers Fundo de Investimento Imobiliário', 'FII'),
+                                                    ('XPML11', 'XP Malls Fundo de Investimento Imobiliário', 'FII'),
+                                                    ('IRDM11', 'Iridium Recebíveis Imobiliários Fundo de Investimento Imobiliário', 'FII'),
+                                                    ('HGRU11', 'CSHG Renda Urbana Fundo de Investimento Imobiliário', 'FII'),
+                                                    ('CPTS11', 'Capitânia Securities II Fundo de Investimento Imobiliário', 'FII');
+
